43f231480974bbfa70da9643689b9692fa9330dc
MD Zisan <zisanislam01@gmail.com>
2023-04-07 00:50:41 +0600

    tshirt data loaded

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className=''>
+           <Link  to="/">Home</Link>
+           <Link className='ml-3 text-lg'  to="/review">Reviews</Link>
+           <Link className='ml-3 text-lg' to="/about">About</Link>
+           <Link className='ml-3 text-lg' to="/contact">Contact</Link>
+
+        </nav>
+    );
+};
+
+export default Header;
